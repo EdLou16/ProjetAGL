6f575e313c0c92d5c91064d1257de5dcb05626d9
EdLou16 <edloumendy@gmail.com>
2019-11-20 14:19:13 +0000

    initialize project

--- src/main/java/com/ucad/cours/Calcul.java
+++ src/main/java/com/ucad/cours/Calcul.java
@@ -1,15 +1,40 @@
 package com.ucad.cours;
 
 public class Calcul {
-	
-	
-	public double addition(double a, double b)
-	{
-		return a+b;
+
+	public int addition(int a, int b) {
+		int res = a;
+		if (b > 0) {
+			while (b-- != 0) {
+				res++;
+			}
+		} else if (b < 0) {
+			while (b++ != 0) {
+				res--;
+			}
+		}
+		return res;
 	}
-	
-	public double multiplication(double a, double b)
-	{
-		return a*b;
+
+	public int multiplication(int a, int b) {
+		if(a==0||b==0) return 0;
+		int res = a;
+		if (b > 0) {
+			while (--b != 0) {
+				res = res + a;
+			}
+		} else if (b < 0  && a >0) {
+			res=-1*res;
+			while (++b != 0) {
+				res = res -a;
+			}
+		}else if (b < 0  && a <0) {
+			res=-1*res;
+			while (++b != 0) {
+				res = res -a;
+			}
+		}
+
+		return res;
 	}
 }
